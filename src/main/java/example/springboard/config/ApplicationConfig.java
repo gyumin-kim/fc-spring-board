18f7ab305245141fff4e1992f42940588232571d
Gyumin Kim <starkying@gmail.com>
2018-10-29 10:58:38 +0900

    Config 설정

---
+++ src/main/java/example/springboard/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package example.springboard.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = { "example.springboard.dao",
+                                "example.springboard.service"})
+@Import({ DBConfig.class })
+// 비즈니스 객체들을 생성하는 Root Container(부모 컨테이너)
+public class ApplicationConfig {
+
+}
---
+++ src/main/java/example/springboard/config/WebMvcContextConfiguration.java
@@ -0,0 +1,41 @@
+package example.springboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"example.springboard.controller"})
+// DispatcherServlet이 생성한 Container
+// Root Container가 생성한 객체(Service, DAO)를 사용한다.
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/images/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    // 원래 WEB-INF 폴더는 브라우저에서 접근할 수 없지만,
+    // InternalResourceViewResolver를 설정하면 WEB-INF 폴더 내의 JSP 파일을 view 화면으로 사용 가능
+    // InternalResourceViewResolver: UrlBasedViewResolver의 subclass
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
