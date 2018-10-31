854d2dc2255874c27106dc4e7004e6db54b64bb5
Gyumin Kim <starkying@gmail.com>
2018-10-31 20:22:47 +0900

    BoardService 구현(deleteBoard 미완성)

---
+++ src/main/java/example/springboard/service/BoardService.java
@@ -0,0 +1,18 @@
+package example.springboard.service;
+
+import example.springboard.dto.Board;
+
+import java.util.List;
+
+public interface BoardService {
+    List<Board> showBoardListAll(Long categoryId);
+    List<Board> showBoardListByMember(Long categoryId, Long memberId);
+    List<Board> showBoardListByTitle(Long categoryId, String title);
+    List<Board> showBoardListByContent(Long categoryId, String content);
+    List<Board> showBoardListByTitleOrContent(Long categoryId, String titleOrContent);
+    Board showBoardDetail(Long id);
+    Board writeBoard(Board board);
+    int updateBoard(Board board);
+    int deleteBoard(Board board);
+    int writeBoardReply(Long id, Board board);
+}
