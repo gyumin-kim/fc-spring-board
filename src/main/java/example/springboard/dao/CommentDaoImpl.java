dff8538383b1965af6745a6922b22267bee4c5be
jang <epfvkdlxj@naver.com>
2018-10-31 10:25:15 +0900

    "private NamedParameterJdbcTemplate 변수명 jdbc - > jdbcTemplate 변경; private SimpleJdbcInsert 변수명 insertAction- > jdbcInsert변경;"

--- src/main/java/example/springboard/dao/CommentDaoImpl.java
+++ src/main/java/example/springboard/dao/CommentDaoImpl.java
@@ -14,14 +14,14 @@ import java.util.*;
 
 @Repository
 public class CommentDaoImpl implements CommentDao {
-    private NamedParameterJdbcTemplate jdbc;
-    private SimpleJdbcInsert insertAction;
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert jdbcInsert;
 
 
     public  CommentDaoImpl(DataSource dataSource){
         System.out.println("CommentDaoImpl()");
-        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
-        this.insertAction = new SimpleJdbcInsert(dataSource).withTableName("comment");
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("comment");
     }
     @Override
     public List<Comment> getCommentList(Long boardId) {
@@ -32,7 +32,7 @@ public class CommentDaoImpl implements CommentDao {
         try{
             RowMapper<Comment> rowMapper = BeanPropertyRowMapper.newInstance(Comment.class);
             Map<String, ?> params = Collections.singletonMap("board_id", boardId);
-            return jdbc.query(sql,params,rowMapper);
+            return jdbcTemplate.query(sql,params,rowMapper);
         }catch (Exception ex){
             System.out.println("리스트를 가져오지 못하였습니다.");
             return null;
@@ -42,7 +42,7 @@ public class CommentDaoImpl implements CommentDao {
     @Override
     public int addComment(Comment comment) {
         SqlParameterSource params = new BeanPropertySqlParameterSource(comment);
-        return insertAction.execute(params);
+        return jdbcInsert.execute(params);
     }
 
     @Override
@@ -56,13 +56,13 @@ public class CommentDaoImpl implements CommentDao {
 //        map.put("member_id",comment.getMemberId());
 //        map.put("reg_date",comment.getRegdate());
         SqlParameterSource params = new BeanPropertySqlParameterSource(comment);
-        return jdbc.update(sql,params);
+        return jdbcTemplate.update(sql,params);
     }
 
     @Override
     public int deleteComment(Long id) {
         Map<String, Long> params = Collections.singletonMap("id",id);
         String sql = "UPDATE comment SET is_deleted = 1 WHERE id = :id";
-        return jdbc.update(sql,params);
+        return jdbcTemplate.update(sql,params);
     }
 }
